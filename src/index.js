0ba1364a4df1e13492afa101945cd88c1dff4c02
Yury Chajkou <yury80@gmail.com>
2018-09-16 17:44:33 +0300

    Cels to Far convert: Phisical limitations removed.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 function warmup(temperature) {
-           
-  if (temperature < Math.pow(10, 32)*1.416808){
-    
-   if (temperature > -273.15){ 
-    
-return Math.round(temperature*1.8+32)
+ return Math.round(temperature*1.8+32)
 };
-  }    
-};
-
