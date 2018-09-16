325c68af08a7eac791fc672404223d5f950cf71f
Yury Chajkou <yury80@gmail.com>
2018-09-16 17:35:45 +0300

    Cels to Far convert: Rounding added, absolute zero and Plank temperature limitations added.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-module.exports = function warmup(temperature) {
-  // your implementation here
-  return temperature*1.8+32
+function warmup(temperature) {
+           
+  if (temperature < Math.pow(10, 32)*1.416808){
+    
+    if (temperature > -273.15){
+    
+return Math.round(temperature*1.8+32)}
+    }    
 };
